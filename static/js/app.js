9a18bab40fa58b8d6ce194e0ad8e2f76c8d788a1
zach-hb <zachblack03@gmail.com>
2023-06-11 22:26:24 -0500

    pushing some changes

--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,16 @@ d3.json(url).then(function(response) {
     console.log(response)
     // get sample values from samples array in response json
     let samples = response.samples;
-    let sampleValues = samples.map(sample => sample.sample_values);
+    let sampleValues = samples.map(sample => sample.sample_values.slice(0,10));
     // set the label values
-    let labels = samples.map(sample => sample.otu_ids);
+    let otuIDs = samples.map(sample => sample.otu_ids.slice(0,10));
+    // create text for chart
+    let otuLabels = samples.map(sample => sample.otu_labels.slice(0,10));
     // trace for sample data
     let trace1 = {
-        x: sampleValues,
-        y: labels,
+        x: sampleValues[0],
+        y: otuIDs[0],
+        text: otuLabels,
         type: 'bar',
         orientation: 'h'
     };
